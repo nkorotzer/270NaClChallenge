8f60e9bf878d61b20699eb024d6372eea5c163f1
Nick Korotzer <nick.korotzer@gmail.com>
2023-04-05 13:30:30 -0700

    part1 first solution

--- part1/part1.py
+++ part1/part1.py
@@ -1,7 +1,16 @@
-import sys
+def xor_bytes(ba1, ba2):
+    return bytes(byte_a ^ byte_b for byte_a, byte_b in zip(ba1, ba2))
 
 def main():
-	pass
+    with open('part1.ciphertext.bin','rb') as ciphertextFile, open('part1.otp.bin','rb') as otpFile:
+        ct = ciphertextFile.read()
+        #print(len(ct))
+        
+        otp = otpFile.read()
+        #print(len(otp))
+        
+        result = xor_bytes(ct, otp)
+        print(result)
 	
 	
 if __name__ == "__main__":
