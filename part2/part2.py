9aa1359c36f98211587d4661d09f2571f9f2b9e4
Nick <nick.korotzer@gmail.com>
2023-04-13 12:38:16 -0700

    part2 solution

--- part2/part2.py
+++ part2/part2.py
@@ -1,5 +1,15 @@
+import nacl.secret
+import nacl.utils
+
+
 def main():
-	pass
+	with open('part2.key.bin',"rb") as keyFile, open('part2.ciphertext.bin',"rb") as ctFile:
+		key = keyFile.read()
+		ct = ctFile.read()
+
+	box = nacl.secret.SecretBox(key)
+	pt = box.decrypt(ct)
+	print(pt)
 	
 if __name__ == "__main__":
 	main()
